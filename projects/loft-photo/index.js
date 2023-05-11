5d3958b8eed12b99b9e3d1cfedb5e85d75834346
Kretov Kirill <Kretovkir526@mail.ru>
2023-05-12 01:38:35 +0300

    final-project week-3 homework completed

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -4,6 +4,6 @@ import('./styles.css');
 const pageNames = ['login', 'main', 'profile'];
 
 document.addEventListener('click', () => {
-    let page =  getRandomElement(pageNames);
-    pages.openPage(page);
+    let randomPage =  getRandomElement(pageNames);
+    pages.openPage(randomPage);
 });
