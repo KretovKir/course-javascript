72257cbccb85678f62e69536e927ccca0125618c
Kretov Kirill <Kretovkir526@mail.ru>
2023-05-31 23:53:47 +0300

    homework week-6 completed

--- projects/loft-photo/mainPage.js
+++ projects/loft-photo/mainPage.js
@@ -5,10 +5,11 @@ import profilePage from './profilePage';
 export default {
   async getNextPhoto() {
     const { friend, id, url } = await model.getNextPhoto();
-    this.setFriendAndPhoto(friend, id, url);
+    const photoStats = await model.photoStats(id);
+    this.setFriendAndPhoto(friend, id, url, photoStats);
   },
 
-  setFriendAndPhoto(friend, id, url) {
+  setFriendAndPhoto(friend, id, url, stats) {
     const photoComp = document.querySelector('.component-photo');
     const headerPhotoComp = document.querySelector('.component-header-photo');
     const headerNameComp = document.querySelector('.component-header-name');
@@ -17,8 +18,10 @@ export default {
     headerNameComp.innerText = `${friend.first_name ?? ''} ${friend.last_name ?? ''}`;
     photoComp.style.backgroundImage = `url(${url})`;
     footerPhotoComp.style.backgroundImage = `url('${model.me.photo_50}')`;
-
+    this.setLikes(stats.likes, stats.liked);
+    this.setComments(stats.comments);
     this.friend = friend;
+    this.photoId = id; 
     
   },
 
@@ -49,5 +52,64 @@ export default {
       await profilePage.setUser(model.me);
       pages.openPage('profile');
     }) ;
+
+    document
+    .querySelector('.component-footer-social-likes')
+    .addEventListener('click', async ()=>{
+      const {likes, liked} = await model.like(this.photoId);
+      this.setLikes(likes, liked);
+    });
+
+    
+    document
+    .querySelector('.component-footer-social-comments')
+    .addEventListener('click', async ()=>{
+      document.querySelector('.component-comments').classList.remove('hidden');
+      await this.loadComments(this.photoId);
+    });
+    const input = document.querySelector('.component-comments-container-form-input');
+    document.querySelector('.component-comments').addEventListener('click', (e)=>{
+      if (e.target === e.currentTarget){
+        document.querySelector('.component-comments').classList.add('hidden')
+      }
+    })
   },
+
+  
+  async loadComments(photo){
+    const comments = await model.getComments(photo);
+    const commentsElements = commentsTemplate({
+      list: comments.map((comment)=>{
+        return {
+          name: `${comment.user.first_name ?? ''} ${comment.user.last_name ?? ''}`,
+          photo: comment.user.photo_50,
+          text: comment.text,
+        }
+      })
+    })
+    document.querySelector('.component-comments-container-list').innerHTML='';
+    document.querySelector('.component-comments-container-list').append(commentsElements);
+    this.setComments(comments.length);
+  },
+
+  setLikes(total, liked){
+    const likesElement = document.querySelector('.component-footer-container-social-likes');
+    likesElement.innerText = total;
+
+    if(liked){
+      likesElement.classList.add('liked');
+    }else{
+      likesElement.classList.remove('liked');
+    }
+  },
+
+  setComments(total){
+    const likesElement=document.querySelector('.component-footer-container-social-comments');
+    likesElement.innerText = total;
+  },
+
+  
+
+  
+
 };
